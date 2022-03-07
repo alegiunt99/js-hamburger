fe3f0f502319e98bceb469abd28724a364a24bc3
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-07 14:41:15 +0100

    Aggiungo pulsanti di apertura e chiusura del menu

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // Mostrare la lista del menu al click del menu ad hamburger
 
+
+// lista dei link
 const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+
+
+// pulsante per aprire la tendina dei link
+const openMenuBtn = document.querySelector(".header-right .fa-bars");
+
+
+// pulsante per chiudere la tendina dei link
+const closeMenu = document.querySelector(".header-right .close");
 
