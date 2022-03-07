c4f2a0dc2848d5a08c30e1b31f4d2ea4c06081dd
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-07 15:13:32 +0100

    Ho attivato il pulsante dell'hamburger menu
    Per aprire il menu a tendina

--- main.js
+++ main.js
@@ -2,13 +2,21 @@
 
 
 // lista dei link
-const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 
 
 // pulsante per aprire la tendina dei link
-const openMenuBtn = document.querySelector(".header-right .fa-bars");
+const openMenuBtn = document.querySelector(".header-right a .fa-bars");
 
 
 // pulsante per chiudere la tendina dei link
-const closeMenu = document.querySelector(".header-right .close");
+const closeMenu = document.querySelector(".hamburger-menu .close");
 
+
+// attivare l'apertura del menu a tendina
+
+openMenuBtn.addEventListener('click',
+    function () {
+    hamburgerMenu.style.display = 'block';
+    openMenuBtn.style.display = 'none';
+})
